309bf81065ce87c89c09feac33d8793d06b28e2d
riasvdv <rias@spatie.be>
2022-05-05 13:26:43 +0000

    Add namespace fix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,6 @@
 
 use Spatie\MailcoachEditor\Http\Controllers\EditorController;
 
-Route::post('render', [EditorController::class, 'render']);
-Route::post('upload', [EditorController::class, 'upload']);
+Route::post('render', ['\\' . EditorController::class, 'render']);
+Route::post('upload', ['\\' . EditorController::class, 'upload']);
 
